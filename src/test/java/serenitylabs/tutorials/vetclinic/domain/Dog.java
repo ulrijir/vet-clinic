0ee0614b6d001fca641566e87beac95df3ff451b
ulrijir <ulrijir@users.noreply.github.com>
2020-10-14 00:42:43 +0200

    Dog class made inmuttable

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -3,32 +3,35 @@ package serenitylabs.tutorials.vetclinic.domain;
 import java.time.LocalDateTime;
 
 public class Dog {
-private String name;
-private String breed;
-private LocalDateTime dateOfBirth;
+	private final String name;
+	private final String breed;
+	private final LocalDateTime dateOfBirth;
+	private final String favouriteFood;
 
-public String getBreed() {
-	return breed;
-}
+	public Dog(String name, String breed, LocalDateTime dateOfBirth, String favouriteFood) {
+		this.name = name;
+		this.breed = breed;
+		this.dateOfBirth = dateOfBirth;
+		this.favouriteFood = favouriteFood;
+	}
 
-public void setBreed(String breed) {
-	this.breed = breed;
-}
+	public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+		this(name, breed, dateOfBirth, null);
+	}
 
-public LocalDateTime getDateOfBirth() {
-	return dateOfBirth;
-}
+	public String getBreed() {
+		return breed;
+	}
 
-public void setDateOfBirth(LocalDateTime dateOfBirth) {
-	this.dateOfBirth = dateOfBirth;
-}
+	public LocalDateTime getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-public String getName() {
-	return name;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
-
+	public String getFavouriteFood() {
+		return favouriteFood;
+	}
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
@@ -16,14 +16,20 @@ public class WhenCreatingANewDog {
 	public void it_should_have_a_name() {
 		LocalDateTime t = LocalDateTime.now();
 		
- 		Dog d = new Dog();
-		d.setName("Alf");
-		d.setBreed("Labrador");
-		d.setDateOfBirth(t);
+ 		Dog d = new Dog("Alf", "Labrador", t);
 		
 		assertEquals("Alf", d.getName());
 		assertEquals("Labrador", d.getBreed());
 		assertEquals(t, d.getDateOfBirth());
+	}
+	
+	@Test
+	public void it_should_have_an_optional_favourite_food() {
+		LocalDateTime t = LocalDateTime.now();
+		
+ 		Dog d = new Dog("Alf", "Labrador", t, "steak");
+
+		assertEquals("steak", d.getFavouriteFood()); 		
 	}
 	
 	
